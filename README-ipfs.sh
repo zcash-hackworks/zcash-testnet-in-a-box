0a5f0286ca552bbacb6c26ef90b37fc9bbce79bc
Ben Wilson <ben@z.cash>
2020-11-02 18:05:23 -0500

    Update the install to start to use kustomize

--- README-ipfs.sh
+++ README-ipfs.sh
@@ -6,26 +6,23 @@ kind delete cluster --name zcash-testnet-in-a-box || true
 kind create cluster --name zcash-testnet-in-a-box
 kubectl cluster-info
 
-kubectl apply -f tekton/releases/tekton-pipelines-v0.13.2.yml
-kubectl apply -f tekton/releases/tekton-dashboard-v0.7.0.yml
-kubectl apply -f tekton/serviceAccount.yml
+kubectl apply -f bases/tekton/releases/tekton-pipelines-v0.13.2.yml
+kubectl apply -f bases/tekton/releases/tekton-dashboard-v0.7.0.yml
 
-kubectl create -f tekton/tasks/create-zcashrpc-secret.yml
-kubectl create -f tekton/tasks/create-monitoring-grafana-admin-secret.yml
+kubectl apply -k bases/
 
-kubectl apply -f ipfs/stateful-set.yml
-kubectl apply -f monitoring/configmap.yml
-kubectl apply -f monitoring/service.yml
-kubectl apply -f monitoring/serviceaccount.yml
-kubectl apply -f monitoring/statefulset.yml
+## Deploy dashboard
+kubectl create configmap grafana-dashboard-zcash-tnb --from-file=./bases/monitoring/grafana-dashboard-zcash-tnb.json
 
-kubectl create configmap grafana-dashboard-zcash-tnb --from-file=./monitoring/grafana-dashboard-zcash-tnb.json
+# Finish tasks I can't automate yet
+kubectl create -f taskruns/create-zcashrpc-secret.yml
+kubectl create -f taskruns/create-monitoring-grafana-admin-secret.yml
 
-kubectl apply -f deploy/configmaps-tnb.yml
-kubectl apply -f tekton/tasks/ipfs-pin-cid.yml
-kubectl create -f tekton/tasks/import-zcash-params-ipfs.yml
-kubectl create -f tekton/tasks/import-zcash-tnb-artifacts-ipfs.yml 
-kubectl create -f tekton/tasks/import-zcash-snapshot-ipfs.yml
+# Populate IPFS
+kubectl create -f taskruns/import-zcash-params-ipfs.yml
+kubectl create -f taskruns/import-zcash-tnb-artifacts-ipfs.yml 
+kubectl create -f taskruns/import-zcash-snapshot-ipfs.yml
+
 
 ## Watch the taskrun statuses
 kubectl get taskruns
